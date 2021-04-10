7740abbc93e8fa967c80ab7b0733a8217ce2a9bd
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-04-10 13:56:24 -0400

    modules

---
+++ import-mod.py
@@ -0,0 +1,8 @@
+import modulePractice as any
+
+any.new("Will")
+
+x = any.player["name"]
+
+print(x)
+
---
+++ modulePractice.py
@@ -0,0 +1,7 @@
+def new(x):
+    print("Hi, " + x)
+
+player = {
+    "name": "Leo",
+    "age": 33
+}
