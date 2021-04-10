1f0e5afee594ed60c1d8c75e741cbcc92e941d23
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-04-10 11:00:38 -0400

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+#conditional if statement
+x = 10
+y = 7
+
+if x > y:
+    print("Messi is the GOAT")
+elif y > x:
+    print("Ronaldo is better")
+else:
+    print("Maradonna is better than both")
+print("Debate finished")
