070d0820f8abe5e8a256e2dfa0e38add8e54c4e4
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-04-10 11:47:11 -0400

    methods and constructors

--- classes.py
+++ classes.py
@@ -3,6 +3,8 @@ class Pet:
     def __init__(self, name, animal):
         self.name = name
         self.animal = animal
+    def new(self):
+        print("My name is " + self.name + ". I am a " + self.animal)
 
 newPet = Pet("Bingo", "Chihuahua")
 newestPet = Pet("Gracie", "Goldendoodle")
@@ -11,6 +13,8 @@ newPet.name = "Flare"
 newPet.animal = "Cairn Terrier"
 
 del newestPet
+
+newPet.new()
 
 print(newPet.name)
 print(newPet.animal)
